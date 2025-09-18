5f1191458d8755e46741e0bc1c1e3ab60d26272b
Pedro Ubeda <36293892+BeLc3bU@users.noreply.github.com>
2025-09-18 20:29:02 +0200

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // Escanea todos los archivos HTML en la raíz
+  theme: {
+    extend: {
+      colors: {
+        stone: {
+            100: '#f5f5f4',
+            200: '#e7e5e4',
+        },
+        slate: {
+            500: '#64748b',
+            600: '#475569',
+            800: '#1e293b',
+            900: '#0f172a',
+        },
+        orange: {
+            600: '#ea580c',
+            700: '#c2410c',
+            800: '#9a3412',
+            900: '#7c2d12',
+        }
+      }
+    },
+  },
+  plugins: [],
+}
