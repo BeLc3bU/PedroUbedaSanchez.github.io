43a9760e8d12b63e7e6585a95585ab8948847ca9
Pedro Ubeda <36293892+BeLc3bU@users.noreply.github.com>
2025-12-10 10:44:00 +0100

    Añadir clases de modo oscuro al Layout principal

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -5,7 +5,7 @@ import BackToTop from '../components/BackToTop';
 
 export default function Layout() {
     return (
-        <div className="flex flex-col min-h-screen">
+        <div className="flex flex-col min-h-screen bg-stone-100 dark:bg-slate-900 text-slate-800 dark:text-slate-200 transition-colors duration-300">
             <Header />
             <main id="main-content" className="flex-grow">
                 <Outlet />
